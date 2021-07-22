7dc7cd4f7e41dd39b19ebf9c9950bae8903dc4fc
STPC161020-1\Utilisateur <nicolas.tarbouriech2503@gmail.com>
2021-07-22 16:46:03 +0200

    no message

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Events\ModelCreated;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory, Notifiable;
+    protected $fillable = [
+        'title', 
+        'slug', 
+        'seo_title', 
+        'excerpt', 
+        'body', 
+        'meta_description', 
+        'meta_keywords', 
+        'active', 
+        'image', 
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
